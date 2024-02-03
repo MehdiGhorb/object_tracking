d579a2a40cef794b1b017a283e62d65f38100dbe
Mehdi Ghorbani <ghorbanimahdi565@gmail.com>
2024-02-03 18:47:43 +0100

    add ESN

---
+++ src/utils/path.py
@@ -0,0 +1,7 @@
+import os
+
+# Get the current working directory
+MAIN_DIR = '/mnt/c/Users/ghorb/OneDrive/Desktop/object_tracking'
+
+# Define the data directory
+DATA_DIR = os.path.join(MAIN_DIR, 'data')
